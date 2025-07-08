089ae1f7ea039ce716b77592e1eacc802ef90e25
Ashishprajapati2906 <prajapatiashu2906@gmail.com>
2025-07-08 13:16:14 +0530

    Due to lack of resources, the project could not be completed.

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
-      <h3 className='text-xl font-semibold text-black'>Welcome back</h3>
-      <p className='text-xs text-slate-700 mt-[5px] mb-6'>
-        please enter your Details to login <br />
-        <b>19:00 minute tak dek liya hai</b>
-      </p>
-    </div>
+    <AuthLayout>
+      <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Welcome back</h3>
+        <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+          please enter your Details to login <br />
+          <b>19:00 minute tak dek liya hai</b>
+        </p>
+      </div>
+    </AuthLayout>
   )
 }
 
